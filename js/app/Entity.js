e26cb37d5d642f2fa20a79550d9369ff9b439e06
Richard Habeeb <richard.habeeb@gmail.com>
2016-05-05 23:37:23 -0500

    Cumulus cell rotation.

--- js/app/Entity.js
+++ js/app/Entity.js
@@ -1,5 +1,5 @@
 define(['app/Vector', 'app/Circle', 'app/Settings'], function(Vector, Circle, Settings) {
-    return function(sprite, radialCenterPosition) {
+    return function(sprite, radialCenterPosition, rotationBounds) {
         var that = {};
         var bounds = null;
         var velocity = Vector();
@@ -8,6 +8,12 @@ define(['app/Vector', 'app/Circle', 'app/Settings'], function(Vector, Circle, Se
         that.topSpeed = 150;
         that.topAngularSpeed = 1.0;
         that.appliedForce = Vector(); //todo protect
+        if(typeof(rotationBounds) !== "undefined" && rotationBounds > 0) {
+            for(var i = 0; i < 360/rotationBounds; i++) {
+                sprite.addAnimationBounds(i*rotationBounds, i*4, 4, 1);
+            }
+            sprite.currentAnimation = 0;
+        }
 
         if(typeof(radialCenterPosition) !== "undefined")
         {
@@ -42,6 +48,10 @@ define(['app/Vector', 'app/Circle', 'app/Settings'], function(Vector, Circle, Se
                 angularVelocity = (angularVelocity > 0 ? 1 : -1) * Math.min(Math.abs(angularVelocity), that.topAngularSpeed);
                 r.setAngle(r.getAngle() + angularVelocity * elapsedTimeSeconds);
                 sprite.position = Vector(r.x + radialCenterPosition.x - bounds.position.x, r.y + radialCenterPosition.y - bounds.position.y);
+                if(typeof(rotationBounds) !== "undefined" && rotationBounds > 0) {
+                    //todo cleanup
+                    sprite.currentAnimation = (Math.round((180 - 180 * r.getAngle() / Math.PI) / rotationBounds) * rotationBounds + 90) % 360;
+                }
             }
             else {
                 var acceleration = Vector(that.mass * that.appliedForce.x, that.mass * that.appliedForce.y);
--- js/app/Game.js
+++ js/app/Game.js
@@ -30,12 +30,12 @@ define(['app/Vector', 'app/Sprite', 'app/Settings', 'app/Keyboard', 'app/Map', '
 
         var middlePoint = Vector(eggCell.getBoundingCircle().position.x, eggCell.getBoundingCircle().position.y);
 
-        var cumulusCell1Sprite = Sprite("img/Cumulus Cell Growth.png", Vector(middlePoint.x, middlePoint.y - 80), Vector(), 32, 1);
-        var cumulusCell1 = Entity(cumulusCell1Sprite, middlePoint);
+        var cumulusCell1Sprite = Sprite("img/cumulus.png", Vector(middlePoint.x, middlePoint.y - 78), Vector(), 32, 1);
+        var cumulusCell1 = Entity(cumulusCell1Sprite, middlePoint, 15);
         cumulusCell1.setBoundingCircle(Circle(Vector(16, 16), 8));
         map.attachSprite(cumulusCell1Sprite, 2);
         entityManager.add(cumulusCell1);
-        cumulusCell1.appliedForce = Vector(0.1,0);
+        cumulusCell1.appliedForce = Vector(-0.1,0);
 
         // var cumulusCell2Sprite = Sprite("img/Cumulus Cell 15.png", Vector(2 * Settings.tileSize.x, 2 * Settings.tileSize.y), Vector(), 32, 1);
         // var cumulusCell2 = Entity(cumulusCell2Sprite);
